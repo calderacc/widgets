a7effe29706268d712d49fbe28018eb1a3d9d099
Malte Hübner <maltehuebner@gmx.org>
2014-09-02 18:48:46 +0200

    Füge nun Leaflet-Karte mit OpenStreetMap-Layer ein.

--- src/map.js
+++ src/map.js
@@ -1,4 +1,12 @@
 window.onload = function()
 {
     $('#criticalmassin-widget-map-container').append('<div id="criticalmassin-widget-map"></div>');
+
+    var map = L.map('criticalmassin-widget-map');
+    map.setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 18
+    }).addTo(map);
 };
