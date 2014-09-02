42f2d0dff98baaadc2a56c8a28293e4506fbd6e6
Malte Hübner <maltehuebner@gmx.org>
2014-09-02 21:00:48 +0200

    Diesen Formatted-DateTime-Kram brauchen wir wohl doch noch.

--- src/text.js
+++ src/text.js
@@ -16,6 +16,15 @@ window.onload = function()
             {
                 if (city == citySlug)
                 {
+                    var dateTime = new Date(rides[city].dateTime);
+
+                    var formattedDate = (dateTime.getDate() < 10 ? '0' + dateTime.getDate() : dateTime.getDate())  + '.' +
+                        (dateTime.getMonth() + 1 < 10 ? '0' + (dateTime.getMonth() + 1) : (dateTime.getMonth() + 1)) + '.' +
+                        (dateTime.getFullYear());
+
+                    var formattedTime = (dateTime.getHours() < 10 ? '0' + dateTime.getHours() : dateTime.getHours()) + '.' +
+                        (dateTime.getMinutes() < 10 ? '0' + dateTime.getMinutes() : dateTime.getMinutes()) + ' Uhr';
+
                     var textContent = '<strong id="criticalmassin-next-tour-headline">N&auml;chste Tour:</strong>';
                     textContent += '<span id="criticalmassin-next-tour-date"></span>';
                     textContent += '<span id="criticalmassin-next-tour-time"></span>';
