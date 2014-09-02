3d63b40e157fd6a93896d4b647aeafbada1be06c
Malte Hübner <maltehuebner@gmx.org>
2014-09-02 20:59:31 +0200

    Statt einer Karte wird nun das Grundgerüst eines Textes angezeigt.

--- src/text.js
+++ src/text.js
@@ -2,10 +2,6 @@
 
 window.onload = function()
 {
-    $('#criticalmassin-widget-map-container').width(width);
-    $('#criticalmassin-widget-map-container').height(height);
-
-
     $.ajax({
         type: 'GET',
         async: false,
@@ -14,41 +10,18 @@ window.onload = function()
         context: this,
         success: function(data)
         {
-            var map = L.map('criticalmassin-widget-map-container', { zoomControl: zoomControl });
-
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-                maxZoom: 18,
-                detectRetina: true
-            }).addTo(map);
-
             var rides = data.rides;
 
             for (var city in rides)
             {
                 if (city == citySlug)
                 {
-                    var mapCenter = L.latLng(rides[city].latitude, rides[city].longitude);
+                    var textContent = '<strong id="criticalmassin-next-tour-headline">N&auml;chste Tour:</strong>';
+                    textContent += '<span id="criticalmassin-next-tour-date"></span>';
+                    textContent += '<span id="criticalmassin-next-tour-time"></span>';
+                    textContent += '<span id="criticalmassin-next-tour-location"></span>';
 
-                    var marker = marker = L.marker(mapCenter).addTo(map);
-
-                    map.setView(mapCenter, zoomLevel);
-
-                    var dateTime = new Date(rides[city].dateTime);
-
-                    var formattedDate = (dateTime.getDate() < 10 ? '0' + dateTime.getDate() : dateTime.getDate())  + '.' +
-                        (dateTime.getMonth() + 1 < 10 ? '0' + (dateTime.getMonth() + 1) : (dateTime.getMonth() + 1)) + '.' +
-                        (dateTime.getFullYear());
-
-                    var formattedTime = (dateTime.getHours() < 10 ? '0' + dateTime.getHours() : dateTime.getHours()) + '.' +
-                            (dateTime.getMinutes() < 10 ? '0' + dateTime.getMinutes() : dateTime.getMinutes()) + ' Uhr';
-
-                    marker.bindPopup('<b>N&auml;chste Tour</b><br />Datum: ' + formattedDate + '<br />Uhrzeit: ' + formattedTime + '<br />Treffpunkt: ' + rides[city].location);
-
-                    if (showPopup)
-                    {
-                        marker.openPopup();
-                    }
+                    $('#criticalmassin-widget-text-container').html(textContent);
                 }
             }
         }
